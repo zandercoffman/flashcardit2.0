112519df3c2d4c3110f92e7b63a07c9339b80ea2
zandergalvez330 <zandergalvez330@gmail.com>
2025-12-01 03:52:04 +0000

    Update loader

--- src/components/studyplan/StudyPlan.tsx
+++ src/components/studyplan/StudyPlan.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { StudyCarousel } from "./StudyCarousel"
-import { LoaderOne } from "@/components/ui/loader";
+import Loader from "../loader"
 import { StartStudying } from "./StartStudying"
 
 interface Set {
@@ -256,7 +256,7 @@ export default function StudyPlan({ currentSet }: { currentSet: Set }) {
 
   if (mode === "loading") {
     return <div className="w-full h-full flex justify-center items-center">
-      <LoaderOne/>
+      <Loader/>
     </div>
   }
 
