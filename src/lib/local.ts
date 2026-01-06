5da3c6b963d8b8be6d65a957c6a5f2776cd98991
Zander Coffman <zandergalvez330@gmail.com>
2026-01-05 23:44:27 -0700

    fix: Ensure window object is defined before checking hostname in isLocal

--- src/lib/local.ts
+++ src/lib/local.ts
@@ -1,5 +1,5 @@
 const overrideNo = true;
-const isLocal = window.location.hostname === "localhost"
+const isLocal = typeof window !== "undefined" && window.location.hostname === "localhost"
 
 export const print = (thing: string | number) => {
     if (isLocal || !overrideNo) console.log(thing)
