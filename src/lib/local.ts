fc045c13211e186c7303756d5b369460123d0842
Zander Coffman <zandergalvez330@gmail.com>
2026-01-05 23:45:55 -0700

    fix: Remove local environment check from print function

--- src/lib/local.ts
+++ src/lib/local.ts
@@ -1,6 +1,5 @@
 const overrideNo = true;
-const isLocal = typeof window !== "undefined" && window.location.hostname === "localhost"
 
 export const print = (thing: string | number) => {
-    if (isLocal || !overrideNo) console.log(thing)
+    if (!overrideNo) console.log(thing)
 }
