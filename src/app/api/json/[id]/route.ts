e66d1fb1fe079ff49af80d992435838503da8e6a
Zander Coffman <zandergalvez330@gmail.com>
2026-02-07 19:23:00 -0700

    refactor: update GET handler to extract ID from request URL and improve error handling

--- src/app/api/json/[id]/route.ts
+++ src/app/api/json/[id]/route.ts
@@ -1,10 +1,12 @@
 import { AllSets } from "@/lib/AllSets";
 import { NextResponse } from "next/server";
 
-export async function GET(_request: Request, { params }: { params: { id: string } }) {
-  const foundSet = AllSets.find((set) => set.id === params.id);
+export async function GET(request: Request) {
+  const { pathname } = new URL(request.url);
+  const id = pathname.split("/").pop();
+  const foundSet = AllSets.find((set) => set.id === id);
 
-  if (!foundSet) {
+  if (!id || !foundSet) {
     return NextResponse.json({ error: "Set not found" }, { status: 404 });
   }
 
